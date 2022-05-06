809937db837727d6e7029a203b4fdc29cdb39c38
linhle <lklinh@outlook.com>
2022-05-06 21:17:48 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import { useState } from 'react/cjs/react.development';
 
 export default function App() {
+  const [greeting, setGreeting] = useState(false)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>{greeting ? "Hey yo, Linh!" : "hey yo!"}</Text>
+      <Pressable onPress={() => setGreeting(!greeting)} style={styles.button}>
+        <Text>Greets</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  text: {
+    color: "#fff",
+    fontSize: 20,
+  },
+  button: {
+    backgroundColor: '#fff',
+    padding: 10,
+    marginTop: 8,
+  }
 });
---
+++ README.md
@@ -0,0 +1,4 @@
+This is a code-along project.
+I'm trying to learn react native with the help of below vid.
+
+https://www.youtube.com/watch?v=ANdSdIlgsEw&t=1550s
