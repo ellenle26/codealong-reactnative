8c9e486c2ef34811e38c275528845f04107b005d
linhle <lklinh@outlook.com>
2022-05-07 11:30:43 +0700

    create scrollview list and add item when refresh

--- App.js
+++ App.js
@@ -1,36 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, RefreshControl } from "react-native";
+import React from 'react';
 
 const App = () => {
 
+  const [list, setList] = React.useState([
+      { id: 1, name: "item1" },
+      { id: 2, name: "item2" },
+      { id: 3, name: "item3" },
+      { id: 4, name: "item4" },
+      { id: 5, name: "item5" },
+      { id: 6, name: "item6" },
+      { id: 7, name: "item7" },
+      { id: 8, name: "item8" },
+      { id: 9, name: "item9" },
+      { id: 10, name: "item10" },
+      { id: 11, name: "item11" },
+      { id: 12, name: "item12" },
+      { id: 13, name: "item13" },
+      { id: 14, name: "item14" },
+      { id: 15, name: "item15" },
+      { id: 16, name: "item16" },
+      { id: 17, name: "item17" },
+      { id: 18, name: "item18" },
+      { id: 19, name: "item19" },
+      { id: 20, name: "item20" },
+  ]);
+  const [refreshing, setRefreshing] = React.useState(false);
+  
+  const onRefresh = () => {
+    setRefreshing(true);
+    setList([...list, { id: Math.random(), name: "newItem added"}]);
+    setRefreshing(false);
+  }
+
+
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.wrapper1}>
-        <View style={styles.view1}>
-          <Text>1</Text>
-        </View>
-        <View style={styles.view2}>
-          <Text>2</Text>
-        </View>
-        <View style={styles.view3}>
-          <Text>3</Text>
-        </View>
-      </View>
-      <View style={styles.view4}>
-        <Text>4</Text>
-      </View>
-      <View style={styles.view5}>
-        <Text>5</Text>
-      </View>
-      <View style={styles.wrapper2}>
-        <View style={styles.view6}>
-          <Text>6</Text>
-        </View>
-        <View style={styles.view7}>
-          <Text>7</Text>
-        </View>
-      </View>
-      <StatusBar style="auto" />
+      <ScrollView style={styles.wrapper} refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          colors="blue"
+        />
+      }>
+        {list.map((item) => {
+          return (
+            <View key={item.id} style={styles.item}>
+              <Text>{item.name}</Text>
+            </View>
+          );
+        })}
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -39,67 +59,21 @@ export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#000",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  wrapper: {
+    padding: 10,
   },
   text: {
     color: "#fff",
     fontSize: 20,
   },
-  wrapper1: {
+  item: {
     flex: 1,
-    width: "100%",
-    flexDirection: "row",
-  },
-  view1: {
-    flex: 1,
-    backgroundColor: "#F1DDBF",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  view2: {
-    flex: 1,
-    backgroundColor: "#E4AEC5",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  view3: {
-    flex: 1,
+    height: 50,
     backgroundColor: "#92BA92",
     justifyContent: "center",
     alignItems: "center",
-  },
-  view4: {
-    flex: 1,
-    width: "100%",
-    backgroundColor: "#8FBDD3",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  view5: {
-    flex: 1,
-    width: "100%",
-    backgroundColor: "#FFCD38",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  wrapper2: {
-    flex: 10,
-    width: "100%",
-    flexDirection: "row",
-  },
-  view6: {
-    flex: 1,
-    backgroundColor: "#BE8C63",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  view7: {
-    flex: 1,
-    backgroundColor: "#A97155",
-    justifyContent: "center",
-    alignItems: "center",
+    marginBottom: 10
   },
 });
