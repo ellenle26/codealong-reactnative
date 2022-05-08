a1fce923bb0c56f2e3b0bb1fc1fc4e0fcf3d1570
linhle <lklinh@outlook.com>
2022-05-08 23:42:47 +0700

    try flat list and sectio list with nested array

--- App.js
+++ App.js
@@ -1,56 +1,67 @@
-import { StyleSheet, Text, View, SafeAreaView, ScrollView, RefreshControl } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  RefreshControl,
+  FlatList,
+  SectionList,
+} from "react-native";
 import React from 'react';
 
 const App = () => {
 
-  const [list, setList] = React.useState([
-      { id: 1, name: "item1" },
-      { id: 2, name: "item2" },
-      { id: 3, name: "item3" },
-      { id: 4, name: "item4" },
-      { id: 5, name: "item5" },
-      { id: 6, name: "item6" },
-      { id: 7, name: "item7" },
-      { id: 8, name: "item8" },
-      { id: 9, name: "item9" },
-      { id: 10, name: "item10" },
-      { id: 11, name: "item11" },
-      { id: 12, name: "item12" },
-      { id: 13, name: "item13" },
-      { id: 14, name: "item14" },
-      { id: 15, name: "item15" },
-      { id: 16, name: "item16" },
-      { id: 17, name: "item17" },
-      { id: 18, name: "item18" },
-      { id: 19, name: "item19" },
-      { id: 20, name: "item20" },
+  const [sections, setSections] = React.useState([
+    {
+      title: 'Title 1',
+      data: ['Item 1-1', 'Item 1-2'],
+    },
+    {
+      title: 'Title 2',
+      data: ['Item 2-1', 'Item 2-2'],
+    },
   ]);
+
   const [refreshing, setRefreshing] = React.useState(false);
   
   const onRefresh = () => {
     setRefreshing(true);
-    setList([...list, { id: Math.random(), name: "newItem added"}]);
+    const adding_index = sections.length + 1;
+    setSections([...sections,
+    {
+      title: 'Title ' + adding_index,
+      data:
+        [
+          'Item ' + adding_index + '-1',
+          'Item ' + adding_index + '-2'
+        ],
+    }
+    ]);
     setRefreshing(false);
   }
 
 
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.wrapper} refreshControl={
-        <RefreshControl
-          refreshing={refreshing}
-          onRefresh={onRefresh}
-          colors="blue"
-        />
-      }>
-        {list.map((item) => {
-          return (
-            <View key={item.id} style={styles.item}>
-              <Text>{item.name}</Text>
-            </View>
-          );
-        })}
-      </ScrollView>
+    <SafeAreaView>
+      <SectionList
+        keyExtractor={(item, index) => index.toString()}
+        sections={sections}
+        renderItem={({ item }) => (
+            <Text style={styles.text}>{item}</Text>
+        )}
+        renderSectionHeader={({ section }) => (
+          <View style={styles.item}>
+            <Text style={styles.text}>{section.title}</Text>
+          </View>
+        )}
+        refreshControl={
+          < RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+          />
+        }
+      />
     </SafeAreaView>
   );
 }
